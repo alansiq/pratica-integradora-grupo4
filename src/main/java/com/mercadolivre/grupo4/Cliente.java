2ec8a5764cd9b84099a2fbe1d1de0918f0a8f125
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-03-15 18:42:06 -0300

    Created Cliente class

---
+++ src/main/java/com/mercadolivre/grupo4/Cliente.java
@@ -0,0 +1,12 @@
+package com.mercadolivre.grupo4;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String sobrenome;
+}
