cf566b53ae1e9e8321734b96017b1e3bc62bf4fe
marianna-meli <marianna.rodrigues@mercadolivre.com>
2022-03-16 09:59:50 -0300

    Creating clientes collection.

--- src/main/java/com/mercadolivre/grupo4/Cliente.java
+++ src/main/java/com/mercadolivre/grupo4/Cliente.java
@@ -10,5 +10,16 @@ public class Cliente {
     private Long id;
     private String nome;
     private String sobrenome;
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                '}';
+    }
 }
+
+
 
---
+++ src/main/java/com/mercadolivre/grupo4/Main.java
@@ -0,0 +1,19 @@
+package com.mercadolivre.grupo4;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente(1L, "Marianna", "Rodrigues");
+        Cliente cliente2 = new Cliente(2L, "Hugo", "Fellipe");
+        Cliente cliente3 = new Cliente(3L, "Alan", "Siqueira");
+
+        LinkedList<Cliente> clientes = new LinkedList<Cliente>();
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        clientes.add(cliente3);
+
+    }
+
+
+}
